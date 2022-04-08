f592bc6a3e84c09b73ae419829dd5a01fe57d3a5
evgeniixx <adrenoxxxrom@icloud.com>
2022-04-08 19:54:48 +0300

    New Commit

---
+++ Tasks/Module5/src/Solution.java
@@ -0,0 +1,25 @@
+/*
+В предложенной ниже задаче нужно посчитать площадь круга
+по формуле s = pi * rad * rad. В данном коде есть две ошибки.
+
+От вас требуется:
+1. Загрузить код на свой репозиторий в основную ветку,
+2. Создать ветку, в которой вы исправите баг.
+3. Смержить две ветки.
+
+Правильный ответ:
+1. Не импортирован класс Scanner.
+2. Не осуществлено приведение приведение переменной s типа double.
+к типу int
+*/
+
+
+public class Solution {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        Scanner scan = new Scanner(System.in);
+        double rad = scan.nextDouble();
+        double s = pi * Math.pow(rad, 2);
+        System.out.println(s);
+    }
+}
